e13c6211a0bcd54ce62c086f61d910ef86d00a7f
Wasim <98075754+Wassi123@users.noreply.github.com>
2023-05-26 16:19:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "b" {
   bucket = "my-terra-111111111111"
-  acl    = "private"
+  aws_s3_bucket_acl    = "private"
 
   tags = {
     Name        = "My bucket"
